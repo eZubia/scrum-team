7a4026e3896daf1a6a805e6495de76bb59000c95
ezubia <ezubia@blacklabs.mx>
2017-05-27 12:13:28 -0600

    Se agrega primer commit del proyecto.

---
+++ app/src/main/java/brothersideas/mx/scrumteam/LoginActivity.java
@@ -0,0 +1,26 @@
+package brothersideas.mx.scrumteam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btnSignIn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
